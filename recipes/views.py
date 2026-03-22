7f53943db873aaab870093e05037ec99b679de8e
Ahmed Abdikadir <Ahabdikadir3@gmail.com>
2026-03-22 14:28:59 -0500

    Added base template

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,7 @@ from .models import Recipe
 def home(request):
     recipes = Recipe.objects.all()
     return render(request, 'recipes/home.html', {'recipes': recipes})
+
+def recipies(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipies.html', {'recipes': recipes})
