996770a1789f1abe91b87538abb47efccd0d2631
Ahmed Abdikadir <Ahabdikadir3@gmail.com>
2026-03-24 14:25:59 -0500

    feat: added image upload + fixed API pages

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import home
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('recipies/', home, name='recipies'),
+    path("", views.home, name="home"),
+    path("recipes/", views.home, name="recipes"),
+    path("add/", views.add_recipe, name="add_recipe"),
+    path("edit/<int:recipe_id>/", views.edit_recipe, name="edit_recipe"),
+    path("delete/<int:recipe_id>/", views.delete_recipe, name="delete_recipe"),
+    path("save-api-recipe/", views.save_api_recipe, name="save_api_recipe"),
+    path("breakfast/", views.breakfast, name="breakfast"),
+    path("lunch/", views.lunch, name="lunch"),
+    path("dinner/", views.dinner, name="dinner"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,81 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Recipe
+from .forms import RecipeForm
+
 
 def home(request):
-    recipes = Recipe.objects.all()
+    recipes = Recipe.objects.all().order_by('-created_at')
     return render(request, 'recipes/home.html', {'recipes': recipes})
 
-def recipies(request):
-    recipes = Recipe.objects.all()
-    return render(request, 'recipes/recipies.html', {'recipes': recipes})
+
+def add_recipe(request):
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('recipes')
+    else:
+        form = RecipeForm()
+
+    return render(request, 'recipes/add_recipe.html', {'form': form})
+
+
+def edit_recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES, instance=recipe)
+        if form.is_valid():
+            form.save()
+            return redirect('recipes')
+    else:
+        form = RecipeForm(instance=recipe)
+
+    return render(request, 'recipes/edit_recipe.html', {'form': form, 'recipe': recipe})
+
+
+def delete_recipe(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    recipe.delete()
+    return redirect('recipes')
+
+
+def save_api_recipe(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        image_url = request.POST.get('image_url')
+
+        if title and description:
+            Recipe.objects.create(
+                title=title,
+                description=description,
+                image_url=image_url
+            )
+
+    return redirect('recipes')
+
+
+def breakfast(request):
+    url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Breakfast"
+    response = requests.get(url)
+    data = response.json()
+    meals = data.get('meals', [])
+    return render(request, 'recipes/breakfast.html', {'meals': meals})
+
+
+def lunch(request):
+    url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken"
+    response = requests.get(url)
+    data = response.json()
+    meals = data.get('meals', [])
+    return render(request, 'recipes/lunch.html', {'meals': meals})
+
+
+def dinner(request):
+    url = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef"
+    response = requests.get(url)
+    data = response.json()
+    meals = data.get('meals', [])
+    return render(request, 'recipes/dinner.html', {'meals': meals})
